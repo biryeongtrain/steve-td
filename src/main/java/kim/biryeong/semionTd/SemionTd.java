bd23208edd1617c086c3a998dc4269c03b8e6a8d
biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>
2026-04-23 05:46:51 +0900

    initial commit

---
+++ src/main/java/kim/biryeong/semionTd/SemionTd.java
@@ -0,0 +1,10 @@
+package kim.biryeong.semionTd;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SemionTd implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
